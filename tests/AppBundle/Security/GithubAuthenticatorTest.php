7c0a4f87e7b7f86854c319d1a2a590f68e6abc5f
gassama <abdoulkhadry.gassama@adveris.fr>
2018-03-22 12:48:06 +0100

    add test clase and diary template

---
+++ tests/AppBundle/Security/GithubAuthenticatorTest.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: gassama
+ * Date: 22/03/18
+ * Time: 12:47
+ */
+
+namespace Tests\AppBundle\Security;
+
+
+use PHPUnit\Framework\TestCase;
+
+class GithubAuthenticatorTest extends TestCase
+{
+
+}
---
+++ tests/AppBundle/Security/GithubUserProviderTest.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: gassama
+ * Date: 22/03/18
+ * Time: 12:45
+ */
+
+namespace Tests\AppBundle\Security;
+
+
+use PHPUnit\Framework\TestCase;
+
+class GithubUserProviderTest extends TestCase
+{
+
+}
